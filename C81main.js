d8c1fc5c2c0456eda312d156d68cc26dfc1c1490
RushiCodez <94935989+RushiCodez@users.noreply.github.com>
2021-12-05 12:35:04 -0600

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,31 @@
+canvas= document.getElementById("my_canvas");
+ctx= canvas.getContext("2d");
+color="yellow";
+
+ctx.beginpath();
+ctx.strokeStyle=color;
+ctx.linewidth=2;
+ctx.arc(200, 200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function mousedown(e) {
+mouse_x = e.clientX-canvas.offsetLeft;
+mouse_y = e.clientY-canvas.offsetTop;
+console.log(" X = " + mouse_x + ",Y = " + mouse_y);
+circle(mouse_x, mouse_y);
+
+}
+
+function circle(mouse_x,mouse_y) {
+    ctx.beginpath();
+    ctx.strokeStyle=color;
+    ctx.linewidth=2;
+    ctx.arc(mouse_x, mouse_y,40,0,2*Math.PI);
+    ctx.stroke()
+
+
+
+
+}
